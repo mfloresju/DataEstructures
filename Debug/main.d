2bf53f0359fa0c26a27800fabd01de215de30bce
Marco <mfloresju@ulasalle.edu.pe>
2016-10-19 18:57:50 -0500

    DataEstructure Marco F.

---
+++ Debug/main.d
@@ -0,0 +1,21 @@
+main.o: ../main.cpp ../CListSimple.h ../CList.h ../IDataStructure.h \
+  ../CNode.h ../CListDouble.h ../CListCircular.h ../CStack.h ../CTree.h \
+  ../CQueue.h
+
+../CListSimple.h:
+
+../CList.h:
+
+../IDataStructure.h:
+
+../CNode.h:
+
+../CListDouble.h:
+
+../CListCircular.h:
+
+../CStack.h:
+
+../CTree.h:
+
+../CQueue.h:
